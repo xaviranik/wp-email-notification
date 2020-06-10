5251dec6dd83edbc3c69ff3cb02e672e85ff7831
xavir <xaviranik.me@gmail.com>
2020-06-10 12:18:37 +0600

    Posts Email Notification v1.1

--- wd-posts-email-notification.php
+++ wd-posts-email-notification.php
@@ -4,7 +4,7 @@
  * Plugin Name:       WeDevs Posts Email Notification
  * Plugin URI:        https://zabiranik.me
  * Description:       Sends email to admin when a post gets published.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Zabir Anik
@@ -25,7 +25,7 @@ final class WD_Posts_Email_Notification {
      * WD Posts Email Notification Version
      * @var string
      */
-    const version = '1.0.0';
+    const version = '1.1.0';
 
     /**
      * Class Constructor
@@ -66,6 +66,7 @@ final class WD_Posts_Email_Notification {
      * @return void
      */
     public function init_plugin() {
+        $this->register_email_service();
         $this->register_post_published_notification();
     }
 
@@ -84,28 +85,70 @@ final class WD_Posts_Email_Notification {
     }
 
     /**
+     * Registers email service
+     *
+     * @return void
+     */
+    protected function register_email_service() {
+        add_action( 'phpmailer_init', 'phpmailer_init' );
+    }
+
+    /**
      * Registers post publish notification
      *
      * @return void
      */
     public function register_post_published_notification() {
-        add_action( 'publish_post', [ $this, 'send_email_to_admin' ], 10, 2 );
+        add_action( 'publish_post', [ $this, 'notify_for_published_post' ], 10, 2 );
     }
 
     /**
-     * Sends email to admin when a post gets published
+     * Sends email to author and site managers when a post gets published
      *
      * @return void
      */
-    public function send_email_to_admin($post_id, $post) {
-        $author  = $post->post_author;
-        $name    = get_the_author_meta('display_name', $author);
-        $email   = get_the_author_meta('user_email', $author);
-        $title   = $post->post_title;
-        $to[]    = sprintf( '%s <%s>', $name, $email );
-        $subject = sprintf( 'Published: %s', $title );
-        $message = sprintf( 'Hello, %s! Your post "%s" has been published.' . "\n\n", $name, $title );
-        wp_mail( $to, $subject, $message );
+    public function notify_for_published_post($post_id, $post) {
+        $author       = $post->post_author;
+        $author_name  = get_the_author_meta('display_name', $author );
+        $author_email = get_the_author_meta('user_email', $author );
+        $title        = $post->post_title;
+        $to[]         = $author_email;
+        $subject      = sprintf( 'Post published: %s', $title );
+        $message      = sprintf( 'Author        : %s, Post Name: "%s" has been published. View post: %s' . "\n\n", $author_name, $title, get_permalink( $post ) );
+
+        add_filter( 'get_site_manager_email_list', [ $this, 'get_site_manager_email_list' ] );
+        $to = apply_filters( 'get_site_manager_email_list', $to );
+
+        wp_mail( implode(", ", $to), $subject, $message );
+    }
+
+    public function get_site_manager_email_list( $emails ) {
+        $managers = get_users( [
+             'role__in' => [ 'administrator', 'editor' ]
+        ] );
+
+        foreach ( $managers as $manager ) {
+            if ( is_email( $manager->user_email ) ) {
+                $emails[] = $manager->user_email;
+            }
+        }
+        return array_unique( $emails );
+    }
+
+    /**
+     * PHPMailer initialization
+     *
+     * @param PHPMailer $phpmailer
+     * @return void
+     */
+    function phpmailer_init(PHPMailer $phpmailer) {
+        $phpmailer->Host       = 'smtp.test.net';
+        $phpmailer->Port       = 465;
+        $phpmailer->Username   = '';
+        $phpmailer->Password   = '';
+        $phpmailer->SMTPAuth   = true;
+        $phpmailer->SMTPSecure = 'ssl';
+        $phpmailer->IsSMTP();
     }
 }
 
